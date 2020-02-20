271b506cfebe57b6b1a877a83a3a2429e7eab7b3
zhouzhouthezhou <kylezhou2222@gmail.com>
2020-02-20 00:09:38 -0700

    hw1

--- lab3/lab3_script.sh
+++ lab3/lab3_script.sh
@@ -19,7 +19,7 @@ grep -c -P "\d{3}-\d{3}-\d{4}" regex_practice.txt
 
 echo "Number of emails in regex_practice.txt"
 #look for a charstring@charstring
-grep -c ".*@.*" regex_practice.txt
+grep -c "(.&^@)(.&^@)*@.*" regex_practice.txt
 
 echo "List of 303 numbers in regex_practice.txt"
 #look for 303- and 3 numbers and a - and 4 numbers with pcre
